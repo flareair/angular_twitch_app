5ef3dbe8ddca28a86f63eaa7a07f0517a618e186
flareair <flareair0@gmail.com>
2016-04-22 12:16:02 +0300

    Initial

---
+++ client/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import './main.less';
+
+import angular from 'angular';
+import ngRouter from 'angular-animate';
+
+console.log('app!');
+
+export default angular.module('app', [ngRouter]).name;
